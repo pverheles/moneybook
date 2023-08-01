146c7675143acfa65930db85fe9ce962c1daca05
Petro Verheles <petro.verheles@gigaspaces.com>
2023-08-01 22:25:35 +0300

    Initial commit

---
+++ src/main/java/com/moneyrama/MoneyramaApplication.java
@@ -0,0 +1,13 @@
+package com.moneyrama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyramaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyramaApplication.class, args);
+	}
+
+}
