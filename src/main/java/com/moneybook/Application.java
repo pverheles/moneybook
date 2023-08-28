1659cf771cc38d56420d00c220cb4296a5211c31
Petro Verheles <petro.verheles@gigaspaces.com>
2023-08-28 21:10:38 +0300

    Renamed package

--- src/main/java/com/moneybook/Application.java
+++ src/main/java/com/moneybook/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MoneyramaApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MoneyramaApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
