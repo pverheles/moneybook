8a0eab6919c9ce0c81a9199cb139212e2983867f
Petro Verheles <petro.verheles@gigaspaces.com>
2023-10-10 22:23:57 +0300

    Envelope currency

--- src/main/java/com/moneybook/entity/Envelope.java
+++ src/main/java/com/moneybook/entity/Envelope.java
@@ -1,6 +1,9 @@
 package com.moneybook.entity;
 
+import com.moneybook.constants.Currency;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,5 +15,8 @@ import lombok.Setter;
 public class Envelope extends AmountEntity {
 
     private ExpensePlan expensePlan;
+
+    @Enumerated(EnumType.STRING)
+    private Currency currency;
 
 }
--- src/main/java/com/moneybook/entity/ExpensePlan.java
+++ src/main/java/com/moneybook/entity/ExpensePlan.java
@@ -1,10 +1,6 @@
 package com.moneybook.entity;
 
-
-import com.moneybook.constants.Currency;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,8 +10,5 @@ import lombok.Setter;
 @Setter
 @Table(name = "expense_plan")
 public class ExpensePlan extends BaseEntity {
-
-    @Enumerated(EnumType.STRING)
-    private Currency currency;
 
 }
