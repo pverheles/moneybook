5cc96af67d470807cb6c8644b92fbc010fc0de82
Petro Verheles <petro.verheles@gigaspaces.com>
2024-02-19 00:05:53 +0200

    clean up

--- src/main/java/com/moneybook/controller/UserController.java
+++ src/main/java/com/moneybook/controller/UserController.java
@@ -2,12 +2,14 @@ package com.moneybook.controller;
 
 import com.moneybook.dto.UserDto;
 import com.moneybook.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/api/user")
 public class UserController {
 
     private final UserService userService;
@@ -17,7 +19,7 @@ public class UserController {
     }
 
     @PostMapping
-    public void createOrUpdateUser(UserDto userDto) {
+    public void createOrUpdateUser(@Valid @RequestBody UserDto userDto) {
         userService.createOrUpdateUser(userDto);
     }
 
--- src/main/java/com/moneybook/usercontext/UserEmailFilter.java
+++ src/main/java/com/moneybook/usercontext/UserEmailFilter.java
@@ -2,12 +2,16 @@ package com.moneybook.usercontext;
 
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
 @Component
+@Slf4j
 public class UserEmailFilter implements Filter {
+
+    private static final String API_PREFIX = "/api/";
 
     private final UserContextService userContextService;
 
@@ -21,13 +25,25 @@ public class UserEmailFilter implements Filter {
                          FilterChain filterChain) throws IOException, ServletException {
 
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+
+        if (isApiCall(httpServletRequest)) {
+            handleEmailHeader(httpServletRequest);
+        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    private boolean isApiCall(HttpServletRequest httpServletRequest) {
+        return httpServletRequest.getServletPath().startsWith(API_PREFIX);
+    }
+
+    private void handleEmailHeader(HttpServletRequest httpServletRequest) {
         String email = httpServletRequest.getHeader("email");
 
         if (email == null) {
-            throw new UnknownUserException("Unknown user " + email);
+            throw new UnknownUserException("Unknown user");
         }
 
         userContextService.putUserContext(new UserContext(email));
-
     }
 }
